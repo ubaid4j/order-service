6dffb8f84d2371338e7aaaf332e19cfe083365c7
ubaid4j <ubaidshan007@gmail.com>
2023-04-09 08:49:49 +0500

    - fix postman tests

--- src/main/java/dev/ubaid/orderservice/web/OrderController.java
+++ src/main/java/dev/ubaid/orderservice/web/OrderController.java
@@ -4,10 +4,12 @@ import dev.ubaid.orderservice.domain.Order;
 import dev.ubaid.orderservice.domain.OrderService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -25,6 +27,7 @@ public class OrderController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Mono<Order> submitOrder(
         @RequestBody @Valid OrderRequest orderRequest
     ) {
