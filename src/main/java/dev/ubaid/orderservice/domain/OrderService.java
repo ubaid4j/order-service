3a35fb5eb051b49d6814662f95ce81cd1886b78f
ubaid4j <ubaidshan007@gmail.com>
2023-04-15 23:10:22 +0500

    - public order accepted event on accepting an order

--- src/main/java/dev/ubaid/orderservice/domain/OrderService.java
+++ src/main/java/dev/ubaid/orderservice/domain/OrderService.java
@@ -3,26 +3,34 @@ package dev.ubaid.orderservice.domain;
 import dev.ubaid.orderservice.book.Book;
 import dev.ubaid.orderservice.book.BookClient;
 import dev.ubaid.orderservice.event.OrderDispatchedMessage;
+import dev.ubaid.orderservice.order.event.OrderAcceptedMessage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OrderService {
     private final BookClient bookClient;
     private final OrderRepository orderRepository;
+    private final StreamBridge streamBridge;
 
     public Flux<Order> getAllOrders() {
         return orderRepository.findAll();
     }
 
+    @Transactional
     public Mono<Order> submitOrder(String isbn, int quantity) {
         return bookClient.getBookByIsbn(isbn)
             .map(book -> buildAcceptedOrder(book, quantity))
             .defaultIfEmpty(buildRejectedOrder(isbn, quantity))
-            .flatMap(orderRepository::save);
+            .flatMap(orderRepository::save)
+            .doOnNext(this::publishOrderAcceptedEvent);
     }
 
     private static Order buildAcceptedOrder(Book book, int quantity) {
@@ -64,5 +72,15 @@ public class OrderService {
             existingOrder.lastModifiedDate(),
             existingOrder.version()
         );
+    }
+
+    private void publishOrderAcceptedEvent(Order order) {
+        if (!OrderStatus.ACCEPTED.equals(order.status())) {
+            return;
+        }
+        var orderAcceptedMessage = new OrderAcceptedMessage(order.id());
+        log.info("Sending order accepted event with id {}", order.id());
+        var result = streamBridge.send("acceptOrder-out-0", orderAcceptedMessage);
+        log.info("Result of sending data for order with id {}: {}", order.id(), result);
     }
 }
---
+++ src/main/java/dev/ubaid/orderservice/order/event/OrderAcceptedMessage.java
@@ -0,0 +1,6 @@
+package dev.ubaid.orderservice.order.event;
+
+public record OrderAcceptedMessage(
+   Long orderId
+) {
+}
