4b7a62bfcd5e9f75ebc214b35f81ec2692f523c1
ubaid4j <ubaidshan007@gmail.com>
2023-06-05 06:48:03 +0500

    - add err logs

--- src/main/java/dev/ubaid/orderservice/book/BookClient.java
+++ src/main/java/dev/ubaid/orderservice/book/BookClient.java
@@ -1,7 +1,9 @@
 package dev.ubaid.orderservice.book;
 
 import java.time.Duration;
+import java.util.function.Function;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
@@ -10,10 +12,15 @@ import reactor.util.retry.Retry;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class BookClient {
     private static final String BOOKS_ROOT_API = "/api/books/";
+    private static final Function<Exception, Mono<Book>> LOG_ERROR = exp -> {
+        log.error("error during getting book from catalog service", exp);
+        return Mono.empty();
+    };
     private final WebClient webClient;
-    
+
     public Mono<Book> getBookByIsbn(String isbn) {
         return webClient
             .get()
@@ -21,8 +28,8 @@ public class BookClient {
             .retrieve()
             .bodyToMono(Book.class)
             .timeout(Duration.ofSeconds(3), Mono.empty())
-            .onErrorResume(WebClientResponseException.class, exp -> Mono.empty())
+            .onErrorResume(WebClientResponseException.class, LOG_ERROR)
             .retryWhen(Retry.backoff(3, Duration.ofMillis(100)))
-            .onErrorResume(Exception.class, exp -> Mono.empty());
+            .onErrorResume(Exception.class, LOG_ERROR);
     }
 }
