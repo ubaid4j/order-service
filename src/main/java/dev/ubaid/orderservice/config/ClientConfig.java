2d019e30e2c9b456418534d54b5e226a5f703ed4
ubaid4j <ubaidshan007@gmail.com>
2023-06-06 05:29:01 +0500

    - enhancements

--- src/main/java/dev/ubaid/orderservice/config/ClientConfig.java
+++ src/main/java/dev/ubaid/orderservice/config/ClientConfig.java
@@ -1,18 +1,44 @@
 package dev.ubaid.orderservice.config;
 
+import io.netty.handler.logging.LogLevel;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.transport.logging.AdvancedByteBufFormat;
 
 @Configuration
 public class ClientConfig {
-    
+
+    @Bean
+    public boolean isDebug(
+        @Value("#{environment.getProperty('debug') != null && environment.getProperty('debug') != 'false'}")
+        boolean isDebug) {
+        return isDebug;
+    }
+
+    @Bean
+    @ConditionalOnExpression("#{isDebug == true}")
+    public ReactorClientHttpConnector wiretappedConnector() {
+        HttpClient httpClient =
+            HttpClient.create()
+                .wiretap(
+                    this.getClass().getCanonicalName(), LogLevel.DEBUG,
+                    AdvancedByteBufFormat.TEXTUAL);
+        return new ReactorClientHttpConnector(httpClient);
+    }
+
     @Bean
     WebClient webClient(
         ClientProperties props,
-        WebClient.Builder builder
+        WebClient.Builder builder,
+        ReactorClientHttpConnector reactorClientHttpConnector
     ) {
         return builder
+            .clientConnector(reactorClientHttpConnector)
             .baseUrl(props.catalogServiceUri().toString())
             .build();
     }
