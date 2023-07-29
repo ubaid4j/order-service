9f68a7d6e0dccef21baece28457b6ae570b76cbd
ubaid4j <ubaidshan007@gmail.com>
2023-07-29 14:23:05 +0500

    - auth audit tests

--- src/test/java/dev/ubaid/orderservice/order/domain/OrderRepositoryTests.java
+++ src/test/java/dev/ubaid/orderservice/order/domain/OrderRepositoryTests.java
@@ -1,6 +1,7 @@
 package dev.ubaid.orderservice.order.domain;
 
 import dev.ubaid.orderservice.config.DataConfig;
+import dev.ubaid.orderservice.domain.Order;
 import dev.ubaid.orderservice.domain.OrderRepository;
 import dev.ubaid.orderservice.domain.OrderService;
 import dev.ubaid.orderservice.domain.OrderStatus;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -15,6 +17,8 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 import reactor.test.StepVerifier;
+
+import java.util.Objects;
 
 @DataR2dbcTest
 @Import(DataConfig.class)
@@ -49,5 +53,24 @@ public class OrderRepositoryTests {
             .create(orderRepository.save(rejectedOrder))
             .expectNextMatches(order -> order.status().equals(OrderStatus.REJECTED))
             .verifyComplete();
+    }
+    
+    @Test
+    void whenCreateOrderNotAuthenticatedThenNoAuditMetaData() {
+        Order rejectedOrder = OrderService.buildRejectedOrder("1234567890", 1);
+        StepVerifier
+                .create(orderRepository.save(rejectedOrder))
+                .expectNextMatches(order -> Objects.isNull(order.createdBy()) && Objects.isNull(order.lastModifiedBy()))
+                .verifyComplete();
+    }
+    
+    @Test
+    @WithMockUser("ubaid")
+    void whenCreatedOrderAuthenticatedThenAuditMetaData() {
+        Order rejectedOrder = OrderService.buildRejectedOrder("1234567890", 2);
+        StepVerifier
+                .create(orderRepository.save(rejectedOrder))
+                .expectNextMatches(order -> order.createdBy().equals("ubaid") && order.lastModifiedBy().equals("ubaid"))
+                .verifyComplete();
     }
 }
